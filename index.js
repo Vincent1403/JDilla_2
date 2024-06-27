a419ec69eab6665a25eb1962c94d80001b7730c1
Vincent1403 <115629387+Vincent1403@users.noreply.github.com>
2024-06-27 18:59:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,151 +1,208 @@
-const rowsx = document.querySelector(".sequencer").children;
-
+const rows = document.querySelector(".sequencer").children;
+ 
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const kick = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/kick.mp3"),
-	clap = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/clap.mp3"),
-	hihat = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/hihat.mp3"),
-	rim = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/rim.mp3"),
-	Q = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/Q.mp3"),
-	W = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/W.mp3"),
-	E = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/E.mp3"),
-	R = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/R.mp3");
-
+    clap = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/clap.mp3"),
+    hihat = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/hihat.mp3"),
+    rim = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/rim.mp3"),
+    Q = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/Q.mp3"),
+    W = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/W.mp3"),
+    E = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/E.mp3"),
+    R = new Audio("https://raw.githubusercontent.com/kucerajacob/DRUM-SEQUENCER/master/audio/R.mp3");
+ 
 const item = document.querySelectorAll(".sample");
-
+ 
 // Checkbox toggle functionality
 item.forEach(function (el) {
-	el.onclick = function () {
-		if (el.classList.contains("item-selected")) {
-			el.classList.remove("item-selected");
-		} else {
-			el.classList.add("item-selected");
-		}
-	}
+    el.onclick = function () {
+        if (el.classList.contains("item-selected")) {
+            el.classList.remove("item-selected");
+        } else {
+            el.classList.add("item-selected");
+        }
+    }
 });
-
+ 
 // Clear button functionality
 document.getElementById("clear-track").onclick = function () {
-	[].forEach.call(item, function (el) {
-		el.classList.remove("item-selected");
-	});
+    [].forEach.call(item, function (el) {
+        el.classList.remove("item-selected");
+    });
 }
-
+ 
 // Sample pad key press functionality
 document.onkeydown = function (e) {
-	e = e || window.event;
-
-	switch (e.key) {
-		case "q":
-			Q.load();
-			Q.play();
-			document.getElementById("sampler1").classList.add("pressed");
-			break;
-		case "w":
-			W.load();
-			W.play();
-			document.getElementById("sampler2").classList.add("pressed");
-			break;
-		case "e":
-			E.load();
-			E.play();
-			document.getElementById("sampler3").classList.add("pressed");
-			break;
-		case "r":
-			R.load();
-			R.play();
-			document.getElementById("sampler4").classList.add("pressed");
-			break;	
-	}
+    e = e || window.event;
+ 
+    switch (e.key) {
+        case "q":
+            playSound(Q);
+            document.getElementById("sampler1").classList.add("pressed");
+            break;
+        case "w":
+            playSound(W);
+            document.getElementById("sampler2").classList.add("pressed");
+            break;
+        case "e":
+            playSound(E);
+            document.getElementById("sampler3").classList.add("pressed");
+            break;
+        case "r":
+            playSound(R);
+            document.getElementById("sampler4").classList.add("pressed");
+            break;    
+    }
 }
-
+ 
 document.onkeyup = function (e) {
-	e = e || window.event;
-
-	switch (e.key) {
-		case "q":
-			// Q.pause();
-			// Q.currentTime = 0;
-			document.getElementById("sampler1").classList.remove("pressed");
-			break;
-		case "w":
-			// W.pause();
-			// W.currentTime = 0;
-			document.getElementById("sampler2").classList.remove("pressed");
-			break;
-		case "e":
-			// E.pause();
-			// E.currentTime = 0;
-			document.getElementById("sampler3").classList.remove("pressed");
-			break;
-		case "r":
-			// R.pause();
-			// R.currentTime = 0;
-			document.getElementById("sampler4").classList.remove("pressed");
-			break;	
-	}
+    e = e || window.event;
+ 
+    switch (e.key) {
+        case "q":
+            document.getElementById("sampler1").classList.remove("pressed");
+            break;
+        case "w":
+            document.getElementById("sampler2").classList.remove("pressed");
+            break;
+        case "e":
+            document.getElementById("sampler3").classList.remove("pressed");
+            break;
+        case "r":
+            document.getElementById("sampler4").classList.remove("pressed");
+            break;    
+    }
 }
-
+ 
 // BPM slider
 const bpmSlider = document.getElementById("bpm-slider");
 const bpmText = document.getElementById("bpm");
-var BPM = bpmSlider.value;
-
+var T0 = parseInt(((60 / bpmSlider.value) * 1000) / 4);
+ 
 bpmText.innerHTML = bpmSlider.value + " BPM";
-
+ 
 bpmSlider.oninput = function () {
-	bpmText.innerHTML = this.value + " BPM";
-	BPM = parseInt(((60 / bpmSlider.value) * 1000) / 4);
+    bpmText.innerHTML = this.value + " BPM";
+    T0 = parseInt(((60 / bpmSlider.value) * 1000) / 4);
 }
-
-let i = -1;
-rowLoop = () => {
-	setTimeout(function () {
-		i++;
-
-		if (i === rowsx.length) {
-			i = 0;
-			document.querySelector(".d16").childNodes[1].classList.remove("row-highlight");
-			document.querySelector(".d16").childNodes[3].classList.remove("row-highlight");
-			document.querySelector(".d16").childNodes[5].classList.remove("row-highlight");
-			document.querySelector(".d16").childNodes[7].classList.remove("row-highlight");
-		}
-
-		document.querySelector(".d" + (i + 1)).childNodes[1].classList.add("row-highlight");
-		document.querySelector(".d" + (i + 1)).childNodes[3].classList.add("row-highlight");
-		document.querySelector(".d" + (i + 1)).childNodes[5].classList.add("row-highlight");
-		document.querySelector(".d" + (i + 1)).childNodes[7].classList.add("row-highlight");
-
-		if (i > 0) {
-			document.querySelector(".d" + i).childNodes[1].classList.remove("row-highlight");
-			document.querySelector(".d" + i).childNodes[3].classList.remove("row-highlight");
-			document.querySelector(".d" + i).childNodes[5].classList.remove("row-highlight");
-			document.querySelector(".d" + i).childNodes[7].classList.remove("row-highlight");
-		}
-
-		document.querySelectorAll(".d" + (i + 1)).forEach(function (bruh) {
-			if (bruh.childNodes[1].classList.contains("row-highlight") && bruh.childNodes[1].classList.contains("item-selected")) {
-				kick.load();
-				kick.play();
-			}
-
-			if (bruh.childNodes[3].classList.contains("row-highlight") && bruh.childNodes[3].classList.contains("item-selected")) {
-				clap.load();
-				clap.play();
-			}
-
-			if (bruh.childNodes[5].classList.contains("row-highlight") && bruh.childNodes[5].classList.contains("item-selected")) {
-				hihat.load();
-				hihat.play();
-			}
-
-			if (bruh.childNodes[7].classList.contains("row-highlight") && bruh.childNodes[7].classList.contains("item-selected")) {
-				rim.load();
-				rim.play();
-			}
-		});
-
-		rowLoop();
-	}, BPM);
+ 
+// SWING slider
+const swingSlider = document.getElementById("swing-slider");
+const swingText = document.getElementById("swing");
+var swing = 1;
+ 
+swingText.innerHTML = "off"
+swingSlider.oninput = function() {
+  if (this.value == 1) {
+    swingText.innerHTML = "off"
+  } else {
+    swingText.innerHTML = "1/"+this.value;
+  }
+  swing = parseFloat(swingSlider.value);
 }
-
-// Call rowLoop() function
-rowLoop();
+ 
+function playSound(audio) {
+    const source = audioContext.createBufferSource();
+    source.buffer = audio.buffer;
+    source.connect(audioContext.destination);
+    source.start();
+}
+ 
+function highlightRow(i) {
+    document.querySelector(".d" + (i + 1)).childNodes[1].classList.add("row-highlight");
+    document.querySelector(".d" + (i + 1)).childNodes[3].classList.add("row-highlight");
+    document.querySelector(".d" + (i + 1)).childNodes[5].classList.add("row-highlight");
+    document.querySelector(".d" + (i + 1)).childNodes[7].classList.add("row-highlight");
+ 
+    if (i > 0) {
+        document.querySelector(".d" + i).childNodes[1].classList.remove("row-highlight");
+        document.querySelector(".d" + i).childNodes[3].classList.remove("row-highlight");
+        document.querySelector(".d" + i).childNodes[5].classList.remove("row-highlight");
+        document.querySelector(".d" + i).childNodes[7].classList.remove("row-highlight");
+    } else {
+        document.querySelector(".d16").childNodes[1].classList.remove("row-highlight");
+        document.querySelector(".d16").childNodes[3].classList.remove("row-highlight");
+        document.querySelector(".d16").childNodes[5].classList.remove("row-highlight");
+        document.querySelector(".d16").childNodes[7].classList.remove("row-highlight");
+    }
+}
+ 
+function scheduleSounds(currentRow, currentTime) {
+    document.querySelectorAll(".d" + (currentRow + 1)).forEach(function (bruh) {
+        if (bruh.childNodes[1].classList.contains("row-highlight") && bruh.childNodes[1].classList.contains("item-selected")) {
+            playSound(kick);
+        }
+ 
+        if (bruh.childNodes[3].classList.contains("row-highlight") && bruh.childNodes[3].classList.contains("item-selected")) {
+            playSound(clap);
+        }
+ 
+        if (bruh.childNodes[5].classList.contains("row-highlight") && bruh.childNodes[5].classList.contains("item-selected")) {
+            playSound(hihat);
+        }
+ 
+        if (bruh.childNodes[7].classList.contains("row-highlight") && bruh.childNodes[7].classList.contains("item-selected")) {
+            playSound(rim);
+        }
+    });
+}
+ 
+var currentTime = audioContext.currentTime;
+let currentRow = 0;
+let j = 0;
+ 
+function loop() {
+    highlightRow(currentRow);
+    scheduleSounds(currentRow, currentTime);
+ 
+    let interval = T0 / 1000;
+ 
+    j++;
+    if (swing !== 1) {
+        const halfSwing = 0.5*swing;
+        if (j === 1) {
+          interval = 2*T0 * Math.ceil(swing / 2) / swing / 1000;
+        } else {
+          interval = 2*T0 * Math.floor(swing/2)/swing / 1000;             
+          j = 0;
+        }
+    }
+    if (j === 2) {
+      j = 0;
+    }
+ 
+    currentTime += interval;
+    currentRow = (currentRow + 1) % rows.length;
+ 
+    setTimeout(loop, interval * 1000);
+}
+ 
+// Convert audio to buffer for scheduling
+function convertToBuffer(audioElement, callback) {
+    const request = new XMLHttpRequest();
+    request.open('GET', audioElement.src, true);
+    request.responseType = 'arraybuffer';
+    request.onload = function () {
+        audioContext.decodeAudioData(request.response, function (buffer) {
+            audioElement.buffer = buffer;
+            if (callback) callback();
+        });
+    }
+    request.send();
+}
+ 
+// Load all audio buffers
+function loadAllBuffers(callback) {
+    let loadedCount = 0;
+    const audioElements = [kick, clap, hihat, rim, Q, W, E, R];
+    audioElements.forEach(audio => {
+        convertToBuffer(audio, () => {
+            loadedCount++;
+            if (loadedCount === audioElements.length) {
+                callback();
+            }
+        });
+    });
+}
+ 
+// Start the loop after all buffers are loaded
+loadAllBuffers(loop);
